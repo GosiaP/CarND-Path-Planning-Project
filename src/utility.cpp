30b6fafd6631df22f64a4d110f407b7fae5b5b1e
GosiaP <30291823+GosiaP@users.noreply.github.com>
2018-06-11 21:59:43 +0200

    fix utility

---
+++ src/utility.cpp
@@ -0,0 +1,11 @@
+#include "utility.h"
+
+
+const double Utility::MIN_HORIZON = 50.0; // [m]
+const double Utility::SPEED_LIMIT = 50.0; // [mph]
+const double Utility::LANE_MARGIN = 0.05; // [m]
+const double Utility::LANE_CHANGE_FRONT_DIST = 15.0; // [m]
+const double Utility::LANE_CHANGE_BACK_DIST = 7.0; // [m]
+const double Utility::INF = std::numeric_limits<double>::infinity();
+const double Utility::ACCELER_LIMIT = 0.16; // [m/s^2]
+
--- src/utility.h
+++ src/utility.h
@@ -1,187 +1,189 @@
-#ifndef UTILITY_H
-#define UTILITY_H
+#ifndef _MY_UTILITY_H
+#define _MY_UTILITY_H
 
 #include <math.h>
 #include <limits>
 #include <algorithm>
+#include <cstddef>
 
-struct FrenetPnt
-{
-  double s;
-  double d;
-};
-
-
-
-struct Utility
-{
-  static constexpr double pi()
+  struct FrenetPnt
   {
-    return 3.14159265358979323846;
-    //return M_PI;
-  }
+    double s;
+    double d;
+  };
 
-  // safe lane distance 
-  static constexpr  double LANE_MARGIN = 0.05; // [m]
 
-  // constant for infinity
-  static constexpr double INF = std::numeric_limits<double>::infinity();
 
-  // paths length -  50 points
-  static constexpr std::size_t PATH_ITEM_COUNT = 50u;
+  struct Utility
+  {
+    static const double pi()
+    {
+      return 3.14159265358979323846;
+      //return M_PI;
+    }
 
-  // safe distance buffer for lane changes in front of me
-  static constexpr double LANE_CHANGE_FRONT_DIST = 15; // [m]
+    // safe lane distance 
+    static const  double LANE_MARGIN;
 
-  // safe distance buffer for lane changes at the back
-  static constexpr double LANE_CHANGE_BACK_DIST = 7; // [m]
+    // constant for infinity
+    static const double INF;
 
-  // horizon in the lane environment model
-  static constexpr double MIN_HORIZON = 50; // [m]
+    // paths length -  50 points
+    static const std::size_t PATH_ITEM_COUNT = 50u;
 
-  // speed limit in mph
-  static constexpr double SPEED_LIMIT = 50.0; // [mph]
+    // safe distance buffer for lane changes in front of me
+    static const double LANE_CHANGE_FRONT_DIST; // [m]
 
-  // target acceleration
-  static constexpr double ACCELER_LIMIT = 0.16; // [m/s^2]
+    // safe distance buffer for lane changes at the back
+    static const double LANE_CHANGE_BACK_DIST; // [m]
+
+    // horizon in the lane environment model
+    static const double MIN_HORIZON; // [m]
+
+    // speed limit in mph
+    static const double SPEED_LIMIT; // [mph]
+
+    // target acceleration
+    static const double ACCELER_LIMIT; // [m/s^2]
 
   
-  // For converting back and forth between radians and degrees.
-  static double deg2rad(double x)
-  {
-    return x * pi() / 180;
-  }
-  static double rad2deg(double x)
-  {
-    return x * 180 / pi();
-  }
-
-  static double mph2mps(double mph)
-  {
-    return mph * 0.44704;
-  }
-
-
-  static double mps2mph(double x)
-  {
-    return x / 0.44704;
-  }
-
-  static double meters2miles(double x)
-  {
-    return x / 1609.34;
-  }
-
-  /**
-  * @brief Retrieves vector length.
-  * @param x vector x component
-  * @param y vector y component
-  * @return |v|
-  */
-  static double v_len(double x, double y)
-  {
-    return sqrt(x * x + y * y);
-  }
-
-  /**
-  * Calculates euclidian distance.
-  * @param x1 - point 1 x
-  * @param y1 - point 1 y
-  * @param x2 - point 2 x
-  * @param y2 - point 2 y
-  * @return distnace.
-  */
-  static double distance(double x1, double y1, double x2, double y2)
-  {
-    return v_len(x2 - x1, y2 - y1);
-  }
-
-  /**
-   * Calculates a difference between two angles (in radians)
-   */
-  static double calc_angle_diff(double  a1, double a2)
-  {
-    const double diff = Utility::normalize_angle(a2) - Utility::normalize_angle(a1);
-    return Utility::normalize_angle(diff);
-  }
-
-  /**
-   * Normalizes angle in radians
-   */
-  static double normalize_angle(double a)
-  {
-    while (a > pi())
+    // For converting back and forth between radians and degrees.
+    static double deg2rad(double x)
     {
-      a -= 2.0f * pi();
+      return x * pi() / 180;
     }
-    while (a < -pi())
+    static double rad2deg(double x)
     {
-      a += 2.0f * pi();
+      return x * 180 / pi();
     }
-    return a;
-  }
 
-  static double get_max_s()
-  {
-    return 6945.554;
-  }
-
-  static double normalize_s(double s_)
-  {
-    while (s_ < 0.) {
-      s_ += get_max_s();
+    static double mph2mps(double mph)
+    {
+      return mph * 0.44704;
     }
-    return s_;
-  }
 
 
-  /**
-   * Calculates vector angle in radians.
-   * @param x - vector x component
-   * @param y - vector y component
-   * @return vector angle.
-   */
-  static double vector_angle(double x, double y)
-  {
-    return atan2(y, x);
-  }
+    static double mps2mph(double x)
+    {
+      return x / 0.44704;
+    }
 
-  /**
-   * Checks if number is near zero (using "epsilon" accuracy)
-   * @param num - floating point number
-   * @return if number is near zero
-   */
-  static bool is_zero(double num)
-  {
-    return fabs(num) < std::numeric_limits<double>::epsilon();
-  }
+    static double meters2miles(double x)
+    {
+      return x / 1609.34;
+    }
+
+    /**
+    * @brief Retrieves vector length.
+    * @param x vector x component
+    * @param y vector y component
+    * @return |v|
+    */
+    static double v_len(double x, double y)
+    {
+      return sqrt(x * x + y * y);
+    }
+
+    /**
+    * Calculates euclidian distance.
+    * @param x1 - point 1 x
+    * @param y1 - point 1 y
+    * @param x2 - point 2 x
+    * @param y2 - point 2 y
+    * @return distnace.
+    */
+    static double distance(double x1, double y1, double x2, double y2)
+    {
+      return v_len(x2 - x1, y2 - y1);
+    }
+
+    /**
+     * Calculates a difference between two angles (in radians)
+     */
+    static double calc_angle_diff(double  a1, double a2)
+    {
+      const double diff = Utility::normalize_angle(a2) - Utility::normalize_angle(a1);
+      return Utility::normalize_angle(diff);
+    }
+
+    /**
+     * Normalizes angle in radians
+     */
+    static double normalize_angle(double a)
+    {
+      while (a > pi())
+      {
+        a -= 2.0f * pi();
+      }
+      while (a < -pi())
+      {
+        a += 2.0f * pi();
+      }
+      return a;
+    }
+
+    static double get_max_s()
+    {
+      return 6945.554;
+    }
+
+    static double normalize_s(double s_)
+    {
+      while (s_ < 0.) {
+        s_ += get_max_s();
+      }
+      return s_;
+    }
 
 
-  // Lane center from the origin (d)
-  static double getLaneCenter_D(int lane_id_)
-  {
-    return (0.5 + lane_id_) * 4.0;
-  }
+    /**
+     * Calculates vector angle in radians.
+     * @param x - vector x component
+     * @param y - vector y component
+     * @return vector angle.
+     */
+    static double vector_angle(double x, double y)
+    {
+      return atan2(y, x);
+    }
 
-  // Lane center with safety margin in border lanes 
-  static double getSafeLaneCenter_D(int lane_id_)
-  {
-    auto c = getLaneCenter_D(lane_id_);
+    /**
+     * Checks if number is near zero (using "epsilon" accuracy)
+     * @param num - floating point number
+     * @return if number is near zero
+     */
+    static bool is_zero(double num)
+    {
+      return fabs(num) < std::numeric_limits<double>::epsilon();
+    }
 
-    if (lane_id_ == 0)
-      c += LANE_MARGIN;
-    else if (lane_id_ == 2)
-      c -= LANE_MARGIN;
 
-    return c;
-  }
+    // Lane center from the origin (d)
+    static double getLaneCenter_D(int lane_id_)
+    {
+      return (0.5 + lane_id_) * 4.0;
+    }
 
-  // Lane at a given distance from the road center
-  static int getLaneId(double d_)
-  {
-    return std::max(0, int(d_) / 4);
-    //return std::floor(d_ / 4.0);
-  }
-};
+    // Lane center with safety margin in border lanes 
+    static double getSafeLaneCenter_D(int lane_id_)
+    {
+      auto c = getLaneCenter_D(lane_id_);
 
-#endif /* TOOLS_H */
+      if (lane_id_ == 0)
+        c += LANE_MARGIN;
+      else if (lane_id_ == 2)
+        c -= LANE_MARGIN;
+
+      return c;
+    }
+
+    // Lane at a given distance from the road center
+    static int getLaneId(double d_)
+    {
+      return std::max(0, int(d_) / 4);
+    }
+  };
+
+
+
+#endif /* _MY_UTILITY_H */
